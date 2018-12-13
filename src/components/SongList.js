aaaededfbd017193d54ff0fcedc4200974b3c7db
Drew <drewharper29@gmail.com>
2018-12-13 15:50:36 -0500

    set up styling on song list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
 
@@ -8,7 +9,8 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button onClick={() => {this.props.selectSong(song)}}
+                            className="ui button primary">Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -26,4 +28,6 @@ const mapStateToProps = (state) => {
     return { songs: state.songs};
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+    selectSong: selectSong
+})(SongList);
